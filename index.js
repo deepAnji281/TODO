eb65383a8c0cd253a6da4d7c4c2946ba1dd2bf36
deepAnji281 <84462559+Deepaksaheb@users.noreply.github.com>
2023-01-16 21:32:08 +0530

    adding some style and css

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();
-const port=8000
+const port=8000;
+app.use('/',require('./routes/index'));
+app.set('view engine','ejs');
+app.set('views','./view');
+app.use(express.static('./assets'));
 app.listen(port,(err)=>{
     if(err){
         console.log(`error occuring in the server run ${err}`);
